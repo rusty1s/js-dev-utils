6af46a858e8e2816cb457c5e67336d907d4c395d
rusty1s <matthias.fey@tu-dortmund.de>
2017-07-14 17:08:08 +0200

    added prettier

--- scripts/eslint.js
+++ scripts/eslint.js
@@ -6,6 +6,7 @@ const { getArgs, mergeArgs, run } = require('../helper/cli');
 const defaultArgs = ['--ext', 'js,jsx,es6', '--cache'];
 
 addDotfile('eslintrc');
+addDotfile('prettierrc');
 addDotfile('eslintignore');
 const code = run('eslint', mergeArgs(defaultArgs, getArgs()));
 removeDotfile('eslintignore');
