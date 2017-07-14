9ab6c905bcb1ab6aa94e537158f6463e7bb010ba
rusty1s <matthias.fey@tu-dortmund.de>
2017-07-14 20:34:48 +1000

    allow default options

--- helper/cli.js
+++ helper/cli.js
@@ -1,14 +1,33 @@
 const path = require('path');
 const { spawnSync } = require('child_process');
 
+const parse = require('yargs-parser');
+
 const { getRoot } = require('./resolve');
 
 const getArgs = () => process.argv.slice(2);
 
-// TODO: Default props.
+const mergeArgs = (...args) => {
+  const allArgs = [].concat(...args);
+  const mergedArgs = Object.assign(...args.map(a => parse(a)));
+
+  return Object.keys(mergedArgs).reduce((acc, key) => {
+    const value = mergedArgs[key];
+
+    if (key === '_') return [...acc, ...value];
+
+    let newKey = key;
+    if (allArgs.includes(`-${key}`)) newKey = `-${key}`;
+    if (allArgs.includes(`--${key}`)) newKey = `--${key}`;
+
+    const arg = typeof value === 'boolean' ? [newKey] : [newKey, value];
+    return [...acc, ...arg];
+  }, []);
+};
+
 const run = (cmd, args = []) =>
   spawnSync('node', [path.join(getRoot(), 'node_modules', '.bin', cmd), ...args], {
     stdio: 'inherit',
   }).status;
 
-module.exports = { getArgs, run };
+module.exports = { getArgs, mergeArgs, run };
--- scripts/eslint.js
+++ scripts/eslint.js
@@ -1,12 +1,12 @@
 #!/usr/bin/env node
 
 const { addDotfile, removeDotfile } = require('../helper/dotfile');
-const { getArgs, run } = require('../helper/cli');
+const { getArgs, mergeArgs, run } = require('../helper/cli');
 
-const defaultOptions = ['--ext', 'js,jsx,es6', '--cache'];
+const defaultArgs = ['--ext', 'js,jsx,es6', '--cache'];
 
 addDotfile('eslintrc');
 addDotfile('eslintignore');
-const code = run('eslint', getArgs());
+const code = run('eslint', mergeArgs(defaultArgs, getArgs()));
 removeDotfile('eslintignore');
 process.exit(code);
