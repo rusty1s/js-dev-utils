0ee6f40a140adb50bbb32d81c51c899b2e172963
rusty1s <matthias.fey@tu-dortmund.de>
2017-07-19 22:53:38 +0200

    dotfile test

--- demo/__tests__/helper/dotfile.js
+++ demo/__tests__/helper/dotfile.js
@@ -1,7 +1,27 @@
+const fs = require("fs");
+const path = require("path");
+
 const { addDotfile, removeDotfile } = require("../../../helper/dotfile");
+const { getRoot } = require("../../../helper/resolve");
 
-test("add dotfile", () => {
-});
+test("add and remove json dotfile", () => {
+  const dir = path.join(getRoot(), "config");
+  const exists = fs.existsSync(dir);
+  if (!exists) fs.mkdirSync(dir);
 
-test("remove dotfile", () => {
+  const filename = path.join(dir, "foo.js");
+  fs.writeFileSync(filename, "module.exports = { foo: true };");
+
+  addDotfile("foo");
+
+  const dotfile = path.join(getRoot(), ".foo");
+  expect(fs.existsSync(dotfile)).toBeTruthy();
+
+  expect(fs.readFileSync(dotfile, "utf8")).toBe('{\n  "foo": true\n}');
+
+  removeDotfile("foo");
+  expect(fs.existsSync(dotfile)).toBeFalsy();
+
+  fs.unlinkSync(filename);
+  if (!exists) fs.rmdirSync(dir);
 });
