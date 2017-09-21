766752245a96aa123915628ab0fab4c1ac365f29
rusty1s <matthias.fey@tu-dortmund.de>
2017-09-21 21:44:58 +0200

    better markdown cli

--- scripts/markdownlint.js
+++ scripts/markdownlint.js
@@ -1,9 +1,8 @@
 #!/usr/bin/env node
 
-const { addDotfile, removeDotfile } = require('../helper/dotfile');
+const { addDotfile } = require('../helper/dotfile');
 const { getArgs, run } = require('../helper/cli');
 
 addDotfile('markdownlintrc');
 const code = run('markdownlint', getArgs());
-removeDotfile('markdownlintrc');
 process.exit(code);
